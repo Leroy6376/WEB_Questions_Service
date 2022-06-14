3753aa6c1cca8f09f6e7e367254cacd560e5517d
Leroy <roza4466@mail.ru>
2022-06-15 00:08:13 +0300

    delete global profile

--- app/views.py
+++ app/views.py
@@ -8,8 +8,6 @@ from . import models
 from django.core.paginator import Paginator
 from django.contrib.auth.models import User
 
-PROFILE = {}
-
 
 # Create your views here.
 
@@ -19,30 +17,28 @@ def index(request):
 def question(request, i:int, page:int):
     QUESTION = models.Question.objects.get(pk=i)
     TAGS = models.TagManager.GetHotTags(9)
-    PROFILE = profile_check(request)
     ANSWERS = models.QuestionManager.GetAnswers(QUESTION)
     p = Paginator(ANSWERS, 4)
     ANSWERS = p.page(page + 1)
     PAGES = get_page_struct(page, p.num_pages)
     
     if request.method == "GET":
-        if PROFILE:
+        if request.user:
             form = AnswerForm()
         else:
             form = AnswerForm(disabled=True)
     elif request.method == "POST":
         form = AnswerForm(request.POST)
         if form.is_valid():
-            form.save(QUESTION, PROFILE)
+            form.save(QUESTION, request.user.profile)
             ANSWERS = models.QuestionManager.GetAnswers(QUESTION)
             p = Paginator(ANSWERS, 4)
             return redirect(reverse("question_url", args=[i, p.num_pages - 1]))
 
-    return render(request, "question.html", {"question" : QUESTION, "answers" : ANSWERS, "tags" : TAGS, "profile" : PROFILE, "page" : PAGES, "form" :form})
+    return render(request, "question.html", {"question" : QUESTION, "answers" : ANSWERS, "tags" : TAGS, "page" : PAGES, "form" :form})
 
 def ask(request):
     TAGS = models.TagManager.GetHotTags(9)
-    PROFILE = profile_check(request)
 
     if request.method == "GET":
         form = QuestionForm()
@@ -58,28 +54,26 @@ def ask(request):
                    
             return redirect(reverse("question_url", args=[question.id, 0]))
 
-    return render(request, "ask.html", {"tags" : TAGS, "profile" : PROFILE, "form" : form})
+    return render(request, "ask.html", {"tags" : TAGS, "form" : form})
 
 
 def profile(request, i:int):
     TAGS = models.TagManager.GetHotTags(9)
-    PROFILE = profile_check(request)
 
     if request.method == "GET":
         initial_data = model_to_dict(request.user)
-        initial_data['avatar'] = PROFILE.avatar
+        initial_data['avatar'] = request.user.profile.avatar
         form = ProfileForm(initial=initial_data)
     elif request.method == "POST":
-        form = ProfileForm(request.POST, request.FILES, instance=PROFILE.user)
+        form = ProfileForm(request.POST, request.FILES, instance=request.user)
         if form.is_valid():
             form.save()
-            return redirect(reverse("profile_url", args=[PROFILE.id]))
+            return redirect(reverse("profile_url", args=[request.user.profile]))
 
-    return render(request, "profile.html", {"tags" : TAGS, "profile" : PROFILE, "form" : form})
+    return render(request, "profile.html", {"tags" : TAGS, "form" : form})
 
 def log_out(request):
     auth.logout(request)
-    PROFILE.clear()
     return redirect(reverse("home"))
 
 def log_in(request):
@@ -118,16 +112,15 @@ def tag(request, j:int, page:int):
     QUESTION = p.page(page + 1)
     PAGES = get_page_struct(page, p.num_pages)
     TAGS = models.TagManager.GetHotTags(9)
-    PROFILE = profile_check(request)
-    return render(request, "tag.html", {"questions" : QUESTION, "tags" : TAGS, "profile" : PROFILE, "page" : PAGES, "id" : j, "name" : Tag.name})
+    return render(request, "tag.html", {"questions" : QUESTION, "tags" : TAGS, "page" : PAGES, "id" : j, "name" : Tag.name})
 
 def hot(request, page:int):
     QUESTION = models.QuestionManager.GetHotQuestions()
-    return question_list(request, page, QUESTION, PROFILE, "Popular questions", "hot")
+    return question_list(request, page, QUESTION, "Popular questions", "hot")
 
 def new_question(request, page:int):
     QUESTION = models.QuestionManager.GetNewQuestions()
-    return question_list(request, page, QUESTION, PROFILE, "New questions", "new")
+    return question_list(request, page, QUESTION, "New questions", "new")
 
 def get_page_struct(page:int, max:int):
     Page_struct = {}
@@ -137,18 +130,10 @@ def get_page_struct(page:int, max:int):
     Page_struct["max"] = max - 1
     return Page_struct
 
-def question_list(request, page, QUESTION, PROFILE, TEXT, LINK):
+def question_list(request, page, QUESTION, TEXT, LINK):
     p = Paginator(QUESTION, 5)
     QUESTION = p.page(page + 1)
     PAGES = get_page_struct(page, p.num_pages)
     TAGS = models.TagManager.GetHotTags(9)
-    PROFILE = profile_check(request)
-    return render(request, "index.html", {"questions" : QUESTION, "tags" : TAGS, "profile" : PROFILE, "page" : PAGES, "text" : TEXT, "link" : LINK})
+    return render(request, "index.html", {"questions" : QUESTION, "tags" : TAGS, "page" : PAGES, "text" : TEXT, "link" : LINK})
 
-
-def profile_check(request):
-    if request.user.is_authenticated:
-        PROFILE = models.Profile.objects.get(user_id=request.user.id)
-    else:
-        PROFILE = {}
-    return PROFILE
